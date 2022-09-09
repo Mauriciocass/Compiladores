e433a88a1e7c1b9ed5d3386492088f112b3226b3
Rob <rcgr.13@gmail.com>
2022-09-08 19:59:35 -0700

    Abrir y leer un archivo, crear y escribir en otro.

--- AnalizadorLexico.py
+++ AnalizadorLexico.py
@@ -1,1 +1,9 @@
-# Prueba de Repositorio
+# Abre el archivo de nombre 'codigo.xxy', lo lee y lo imprime.
+f = open('codigo.xxy', 'r')
+code = f.read()
+print(code)
+
+# Crea un nuevo archivo de nombre 'codigo.xxz' y escribe lo mismo estaba en el
+# archivo 'codigo.xxy'.
+g = open('codigo.xxz', 'w')
+g.write(code)
