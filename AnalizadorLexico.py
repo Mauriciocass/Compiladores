4c864cfec2471a055068d22b7d6584b0cf6a3096
Rob <rcgr.13@gmail.com>
2022-09-09 14:05:35 -0700

    Estructurar codigo para que tenga main.

--- AnalizadorLexico.py
+++ AnalizadorLexico.py
@@ -1,9 +1,22 @@
-# Abre el archivo de nombre 'codigo.xxy', lo lee y lo imprime.
-f = open('codigo.xxy', 'r')
-code = f.read()
-print(code)
+def main():
+    # Abre el archivo de nombre 'codigo.xxy'.
+    f = open('codigo.xxy', 'r')
+    code = f.read()
+    print(code)
 
-# Crea un nuevo archivo de nombre 'codigo.xxz' y escribe lo mismo estaba en el
-# archivo 'codigo.xxy'.
-g = open('codigo.xxz', 'w')
-g.write(code)
+    # Divide el codigo en lineas, para tokenizar linea por linea.
+    code = code.split('\n')
+
+    #
+    for line in code:
+        pass
+
+    # Crea un nuevo archivo de nombre 'codigo.xxz' y escribe lo mismo estaba en el
+    # archivo 'codigo.xxy'.
+    g = open('codigo.xxz', 'w')
+
+    for l in code:
+        g.write(l + '\n')
+
+if __name__=="__main__":
+    main()
