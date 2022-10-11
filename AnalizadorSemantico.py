7ada8a8b751ccc37867cdb386c18bb3cf3cc012e
Rob <rcgr.13@gmail.com>
2022-10-11 15:57:30 -0700

    Ya funcion Desde y Realiza

--- AnalizadorSemantico.py
+++ AnalizadorSemantico.py
@@ -21,22 +21,22 @@ def check_20(code):
 def check_22(code):
     print('revisa DESDE y REALIZA')
     input()
-    if len(code) >= 10:
-        if f'{code[0]}{code[2]}{code[4]}{code[6]}{code[8]}{code[9]}' == '[22][00][27][00][23][13]' and code[1] in variables:
-            for x in list(range(0,10)):
-                code.pop(0)
-            check_code(code)
-        else:
-            print(f'ERROR en {code[0]}{code[1]}')
-            code.pop(0)
-            code.pop(0)
-            # Vaciar pila si hay error.
 
-    if f'{code[0]}' == '[14][23]':
-        code.pop(0)
+    if f'{code[0]}{code[2]}{code[4]}{code[6]}{code[8]}{code[9]}' == '[22][00][27][00][23][13]' and code[1] in variables:
+        for x in list(range(0,10)):
+            code.pop(0)
+        check_code(code)
+    else:
+        print(f'DESDE y REALIZA')
+        print(f'ERROR en {code[0]}{code[1]}')
+        code = []
+        # Vaciar pila si hay error.
+
+    if f'{code[0]}' == '[14]':
         code.pop(0)
     else:
-        print(f'ERROR en {code[0]}{code[1]}')
+        print(f'DESDE y REALIZA 2')
+        print(f'ERROR en {code[0]}')
         code = []
 
 # Revisa los MIENTRAS y REALIZA
@@ -46,29 +46,36 @@ def check_24(code):
 # Revisa los LEE
 def check_25(code):
     print('revisa LEE')
+    input()
     if f'{code[0]}{code[2]}' == '[25][15]' and code[1] in variables:
         code.pop(0)
         code.pop(0)
         code.pop(0)
     else:
+        print(f'LEE')
         print('ERROR! in line:\n' +
         f'{code[0]}{code[1]}{code[2]}')
+        code = []
 
 # Revisa los ESCRIBE
 def check_26(code):
     print('revisa ESCRIBE')
+    input()
     if f'{code[0]}{code[2]}' == '[26][15]' and code[1] in variables:
         code.pop(0)
         code.pop(0)
         code.pop(0)
     else:
+        print(f'ESCRIBE')
         print('ERROR! in line:\n' +
         f'{code[0]}{code[1]}{code[2]}')
+        code = []
         # Vaciar pila si hay error.
 
 # Revisa el INICIO y FIN
 def check_30(code):
     print('revisa INICIO y FIN')
+    input()
     if f'{code[0]}{code[1]}' == '[30][13]':
         code.pop(0)
         code.pop(0)
@@ -77,35 +84,40 @@ def check_30(code):
         code.pop(0)
         code.pop(0)
     else:
+        print(f'INICIO y FIN')
         print(f'ERROR en {code[0]}{code[1]}')
-        code.pop(0)
-        code.pop(0)
+        code = []
 
 
 def check_code(code):
-    while len(code) > 0:
-        print(code)
-        if code[0] == '[20]':
-            check_20(code)
-        elif code[0] == '[22]' or (code[0] == '[14]' and len(code) > 2):
-            check_22(code)
-        elif code[0] == '[24]':
-            check_24(code)
-        elif code[0] == '[25]':
-            check_25(code)
-        elif code[0] == '[26]':
-            check_26(code)
-        elif len(code) == 1 or code[0] == '':
-            print('El codigo se termino de analizar')
-        elif code[0] == '[30]' or code[1] == '[31]':
-            check_30(code)
-        else:
-            print(f'ERROR en {code[0]}')
+    print(code)
+    input()
+    if code[0] == '[20]':
+        check_20(code)
+    elif code[0] == '[22]':
+        check_22(code)
+    elif code[0] == '[24]':
+        check_24(code)
+    elif code[0] == '[25]':
+        check_25(code)
+    elif code[0] == '[26]':
+        check_26(code)
+    elif len(code) == 1 or code[0] == '':
+        print('El codigo se termino de analizar')
+    elif code[0] == '[30]' or code[1] == '[31]':
+        check_30(code)
+    else:
+        print(f'CHECK CODE')
+        print(f'ERROR en {code[0]}')
+        code = []
+    return code
 
 def main():
     code = open('codigo.xxz', 'r').read().replace('][', ']\n[').split('\n')
 
-    check_code(code)
+    while len(code) != 0:
+        code = check_code(code)
+
     print(code)
     print('ANALIZADO')
 
