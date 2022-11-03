aae00a211ff804cae0d90fdb3af0c3ecb7251338
Rob <rcgr.13@gmail.com>
2022-11-03 13:01:31 -0700

    Se genera codigo para Si, Sino y Asignaciones

--- GeneradorCodigo.py
+++ GeneradorCodigo.py
@@ -1,66 +1,34 @@
 import pandas as pd
 import numpy as np
-from binarytree import Node
 
 variables = pd.read_csv('referencias.csv').to_numpy()
 
 variables = np.delete(variables, 0, 1)
 
 contEti = 0
-etiquetas = 0
+printETI = 0
+etiquetas = []
+
+# Carga el codigo del archivo .xxz y lo regresa como lista de tokens
+def cargarCodigo():
+    code = open('codigo.xxz', 'r').read().replace('][', ']\n[').split('\n')
+    code.pop()
+    code.pop()
+    code.pop()
+    code.pop(0)
+    code.pop(0)
+    return code
 
 # Imprime todas las variables que se tienen en la tabla de referencias
 def assemblyVariables():
+    print('\njmp inicio:')
     for variable in variables:
             temp1 = variable[1].replace("[","").replace("]","")
             temp2 = variable[0]
             print(f'    {temp1} {temp2}')
 
-# Imprime la condicion del si
-def assemblySi(code):
-    global contEti
-    global etiquetas
-    print(f'    mov ax, {code[2]}')
-    print(f'    mov bx, {code[4]}')
-    print(f'    cmp ax, bx')
+    print('\ninicio:')
 
-    # ==
-    if(code[3] == '[07]'):
-        print(f'    je ETI{contEti}')
-    # <>
-    elif(code[3] == '[08]'):
-        print(f'    jne ETI{contEti}')
-    # >
-    elif(code[3] == '[09]'):
-        print(f'    jg ETI{contEti}')
-    # >=
-    elif(code[3] == '[10]'):
-        print(f'    jge ETI{contEti}')
-    # <
-    elif(code[3] == '[11]'):
-        print(f'    jl ETI{contEti}')
-    # <=
-    elif(code[3] == '[12]'):
-        print(f'    jle ETI{contEti}')
-
-'''
-    contEti += 1
-    print(f'    jmp ETI{contEti}')
-    contEti += 1
-
-    for x in list(range(0, 7)):
-        code.pop(0)
-
-    ifContent(code)
-
-def ifContent(code):
-    global contEti
-    global etiquetas
-
-    print(f'\nETI{etiquetas}')
-    etiquetas += 1
-    print(f'    jmp ETI{etiquetas}')
-'''
 # Asignacion
 def asignacion(code):
     destiny = code.pop(0)
@@ -75,7 +43,7 @@ def asignacion(code):
     tempCode = posfija(tempCode)
     print(f'    mov {destiny}, ax')
 
-# Convirte una expresion infija a una posfija
+# Convierte una expresion infija a una posfija e imprime las instrucciones en ensamblador
 def posfija(expresion):
     operator = ['[01]', '[02]', '[03]', '[04]']
     prec = {'[01]':1, '[02]':1, '[03]':2, '[04]':2, '[05]':0, '[06]':0}
@@ -140,26 +108,95 @@ def posfija(expresion):
 
     return postfix
 
+def assemblySi(code):
+    global contEti
+    global printETI
+    jumps = {'[07]':'je', '[08]':'jne', '[09]':'jg', '[10]':'jge', '[11]':'jl', '[12]':'jle',}
+
+    tempCode = []
+    brackets = 0
+    # Este codigo revisa el inicio y fin del si, para mandar solo eso a la funcion que crea el codigo de si.
+    for x in code:
+        if x == '[13]':
+            tempCode.append(x)
+            brackets += 1
+        elif x == '[14]':
+            tempCode.append(x)
+            brackets -= 1
+            if brackets == 0:
+                break
+        else:
+            tempCode.append(x)
+    # Quita todo el codigo que fue impreso en el si anteriormente
+    for x in tempCode:
+            code.remove(x)
+
+    print(f'    mov ax, {tempCode[2]}')
+    print(f'    mov bx, {tempCode[4]}')
+    print(f'    cmp ax, bx')
+
+    print(f'    {jumps[tempCode[3]]} ETI{contEti}')
+    contEti += 1
+    print(f'    jmp ETI{contEti}')
+    contEti += 1
+
+    print(f'\nETI{printETI}')
+    printETI += 1
+    switch(tempCode[8:-1])
+
+    if(code[0] == '[21]'):
+        print(f'    jmp ETI{contEti}')
+        assemblySino(code)
+        print(f'\nETI{contEti}:')
+    else:
+        print(f'\nETI{printETI}:')
+        printETI += 1
+
+def assemblySino(code):
+    global printETI
+    tempCode = []
+    brackets = 0
+    # Este codigo revisa el inicio y fin del sino, para mandar solo eso a la funcion que crea el codigo de sino.
+    for x in code:
+        if x == '[13]':
+            tempCode.append(x)
+            brackets += 1
+        elif x == '[14]':
+            tempCode.append(x)
+            brackets -= 1
+            if brackets == 0:
+                break
+        else:
+            tempCode.append(x)
+
+    # Quita todo el codigo que fue impreso en el sino anteriormente
+    for x in tempCode:
+            code.remove(x)
+
+    print(f'\nETI{printETI:}')
+    printETI += 1
+    switch(tempCode[3:-1])
+
+def switch(code):
+    try:
+        # Si hay un si se va a la funcion para imprimir el si
+        if(code[0] == '[20]'):
+            assemblySi(code)
+        # Hace el codigo para las asignaciones
+        elif code[0] in variables:
+            asignacion(code)
+    except:
+        pass
+
 def main():
-    print('\njmp inicio')
+    global contEti
     assemblyVariables()
-    print('\ninicio')
+    code = cargarCodigo()
 
-    code = open('codigo.xxz', 'r').read().replace('][', ']\n[').split('\n')
-    code.pop()
-    code.pop()
-    code.pop()
-    code.pop(0)
-    code.pop(0)
+    while len(code) > 0:
+        switch(code)
 
-    if(code[0] == '[20]'):
-        assemblySi(code)
-    elif code[0] in variables:
-        asignacion(code)
-
-    print('    int 20H')
-
-    # print(code)
+    print('\n    int 20H')
 
 if __name__=="__main__":
     main()
