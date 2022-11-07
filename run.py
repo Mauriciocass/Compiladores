9f22730e6fef5ede20ddffb543bae0c0b5025796
Mauriciocass <51541701+Mauriciocass@users.noreply.github.com>
2022-11-07 08:09:57 -0800

    Revert "Rob"

--- run.py
+++ run.py
@@ -1,11 +1,9 @@
 from AnalizadorLexico import main as lex
 from AnalizadorSemantico import main as sem
-from GeneradorCodigo import main as cod
 
 def main():
     lex()
     sem()
-    # cod()
 
 if __name__=="__main__":
     main()
