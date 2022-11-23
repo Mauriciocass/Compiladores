61abeab10eb7d32938efb92bb2f2d6b780bb0862
13-Rob <rcgr.13@gmail.com>
2022-11-22 20:06:14 -0800

    Programa creo que terminado

--- AnalizadorSemantico.py
+++ AnalizadorSemantico.py
@@ -12,11 +12,21 @@ instrucciones = [
 ]
 
 operadores = [
-    "[06]", "[07]", "[08]", "[09]", "[10]", "[11]", "[12]"
+    "[07]", "[08]", "[09]", "[10]", "[11]", "[12]"
 ]
 
-aritmeticos = {
-    "[00]", "[01]", "[02]", "[03]", "[04]"
+aritmeticos1 = {
+    "[03]", "[04]"
+}
+
+aritmeticos2 = {
+    "[01]", "[02]"
+}
+
+aritmeticos = aritmeticos1.union(aritmeticos2)
+
+brackets = {
+    "[05]", "[06]"
 }
 
 variables = pd.read_csv('referencias.csv')['Value'].to_list()
@@ -139,24 +149,32 @@ def check_asignacion(code):
     global error
     espera_var = True
     c = 0
+    brackets = 0
+    code.pop(0)
 
-    while f'{code[0]}' != '[15]':
-        c += 1
-        if espera_var and f'{code[0]}' in variables:
-            code.pop(0)
-            espera_var = False
-        elif not espera_var and f'{code[0]}' in aritmeticos:
-            code.pop(0)
-            espera_var = True
-        elif c == 2 and code[0] != '[00]':
-            print('ERROR: Asignacion requiere un "="')
-            for x in range(len(code)-1):
+    if f'{code[0]}' == '[00]':
+        code.pop(0)
+        while f'{code[0]}' != '[15]':
+            c += 1
+            if espera_var and f'{code[0]}' in variables:
                 code.pop(0)
-        elif code[0] == '[05]':
-            code.pop(0)
-            check_asignacion(code)
-        else:
-            break
+                espera_var = False
+            elif espera_var and f'{code[0]}' in aritmeticos2:
+                code.pop(0)
+                espera_var = True
+            elif not espera_var and f'{code[0]}' in aritmeticos:
+                code.pop(0)
+                espera_var = True
+            elif code[0] == '[05]':
+                brackets += 1
+                code.pop(0)
+            elif code[0] == '[06]':
+                brackets -= 1
+                code.pop(0)
+            else:
+                break
+    else:
+        print('ERROR: Mala asignacion, falta =')
 
     if espera_var:
         print('ERROR: Asignacion incorrecta')
--- GeneradorCodigo.py
+++ GeneradorCodigo.py
@@ -7,6 +7,7 @@ variables = np.delete(variables, 0, 1)
 contEti = 0
 printETI = 0
 etiquetas = []
+result = ""
 
 # Carga el codigo del archivo .xxz y lo regresa como lista de tokens
 def cargarCodigo():
@@ -20,16 +21,21 @@ def cargarCodigo():
 
 # Imprime todas las variables que se tienen en la tabla de referencias
 def assemblyVariables():
+    global result
+    result += '\njmp inicio:\n'
     print('\njmp inicio:')
     for variable in variables:
             temp1 = variable[1].replace("[","").replace("]","")
             temp2 = variable[0]
+            result += f'    {temp1} {temp2}\n'
             print(f'    {temp1} {temp2}')
 
+    result += '\ninicio:\n'
     print('\ninicio:')
 
 # Asignacion
 def asignacion(code):
+    global result
     destiny = code.pop(0)
     tempCode = []
 
@@ -40,28 +46,48 @@ def asignacion(code):
     code.pop(0)
 
     tempCode = posfija(tempCode)
+    result += f'    mov {destiny}, ax\n'
     print(f'    mov {destiny}, ax')
 
 # Convierte una linea de codigo lee a codigo ensamblador
 def lee(code):
+    global result
     code.pop(0)
+
+    result += '    mov ax, 01\n'
     print('    mov ax, 01')
+
+    result += f'    mov dx, {code[0]}\n'
     print(f'    mov dx, {code.pop(0)}')
+
+    result += f'    mov cx, 2\n'
     print(f'    mov cx, 2')
+
+    result += f'    int 21H\n'
     print(f'    int 21H')
     code.pop(0)
 
 # Convierte una linea de codigo escribe a codigo ensamblador
 def escribe(code):
+    global result
     code.pop(0)
+
+    result += '    mov ax, 02\n'
     print('    mov ax, 02')
+
+    result += f'    mov dx, {code[0]}\n'
     print(f'    mov dx, {code.pop(0)}')
-    print(f'    mov cx, 2')
-    print(f'    int 21H')
     code.pop(0)
+
+    result += f'    mov cx, 2'
+    print(f'    mov cx, 2')
+
+    result += f'    int 21H\n'
+    print(f'    int 21H')
 
 # Convierte una expresion infija a una posfija e imprime las instrucciones en ensamblador
 def posfija(expresion):
+    global result
     operator = ['[01]', '[02]', '[03]', '[04]']
     prec = {'[01]':1, '[02]':1, '[03]':2, '[04]':2, '[05]':0, '[06]':0}
 
@@ -69,61 +95,108 @@ def posfija(expresion):
     stack = []
 
     if len(expresion) == 1:
+        result += f'    move ax, {expresion[-1]}\n'
         print(f'    move ax, {expresion.pop()}')
     else:
         for x in expresion:
             if x not in operator and x != '[05]' and x != '[06]':
+                result += f'    move ax, {x}\n'
                 print(f'    move ax, {x}')
+
+                result += f'    push ax\n'
                 print(f'    push ax')
                 postfix.append(x)
+
             elif x == '[05]':
                 stack.append(x)
             elif x == '[06]':
                 while stack[-1]!= '[05]':
+                    result += f'    pop bx\n'
                     print(f'    pop bx')
+
+                    result += f'    pop ax\n'
                     print(f'    pop ax')
+
                     if(stack[-1] == '[01]'):
+                        result += '    add ax, bx\n'
                         print('    add ax, bx')
+
                     elif(stack[-1] == '[02]'):
+                        result += '    sub ax, bx\n'
                         print('    sub ax, bx')
+
                     elif(stack[-1] == '[03]'):
+                        result += '    mul ax, bx\n'
                         print('    mul ax, bx')
+
                     elif(stack[-1] == '[04]'):
+                        result += '    div ax, bx\n'
                         print('    div ax, bx')
+
                     postfix.append(stack.pop())
                     if len(stack) != 0:
+                        result += '    push ax\n'
                         print('    push ax')
                 stack.pop()
+
             else:
                 while len(stack) != 0 and prec[x] <= prec[stack[-1]]:
+                    result += f'    pop bx\n'
                     print(f'    pop bx')
+
+                    result += f'    pop ax\n'
                     print(f'    pop ax')
+
                     if(stack[-1] == '[01]'):
+                        result += '    add ax, bx\n'
                         print('    add ax, bx')
+
                     elif(stack[-1] == '[02]'):
+                        result += '    sub ax, bx\n'
                         print('    sub ax, bx')
+
                     elif(stack[-1] == '[03]'):
+                        result += '    mul ax, bx\n'
                         print('    mul ax, bx')
+
                     elif(stack[-1] == '[04]'):
+                        result += '    div ax, bx\n'
                         print('    div ax, bx')
+
                     postfix.append(stack.pop())
+
                     if len(stack) != 0:
+                        result += '    push ax\n'
                         print('    push ax')
+
                 stack.append(x)
 
         while len(stack) != 0:
+            result += f'    pop bx\n'
             print(f'    pop bx')
+
+            result += f'    pop ax\n'
             print(f'    pop ax')
+
             if(stack[-1] == '[01]'):
+                result += '    add ax, bx\n'
                 print('    add ax, bx')
+
             elif(stack[-1] == '[02]'):
+                result += '    sub ax, bx\n'
                 print('    sub ax, bx')
+
             elif(stack[-1] == '[03]'):
+                result += '    mul ax, bx\n'
                 print('    mul ax, bx')
+
             elif(stack[-1] == '[04]'):
+                result += '    div ax, bx\n'
                 print('    div ax, bx')
+
             postfix.append(stack.pop())
             if len(stack) != 0:
+                result += '    push ax\n'
                 print('    push ax')
 
     return postfix
@@ -132,6 +205,7 @@ def posfija(expresion):
 def assemblySi(code):
     global contEti
     global printETI
+    global result
     jumps = {'[07]':'je', '[08]':'jne', '[09]':'jg', '[10]':'jge', '[11]':'jl', '[12]':'jle',}
 
     tempCode = []
@@ -152,32 +226,46 @@ def assemblySi(code):
     for x in tempCode:
             code.remove(x)
 
+    result += f'    mov ax, {tempCode[2]}\n'
     print(f'    mov ax, {tempCode[2]}')
+
+    result += f'    mov ax, {tempCode[4]}\n'
     print(f'    mov bx, {tempCode[4]}')
+
+    result += f'    cmp ax, bx\n'
     print(f'    cmp ax, bx')
 
+    result += f'    {jumps[tempCode[3]]} ETI{contEti}\n'
     print(f'    {jumps[tempCode[3]]} ETI{contEti}')
     contEti += 1
+
+    result += f'    jmp ETI{contEti}\n'
     print(f'    jmp ETI{contEti}')
     contEti += 1
 
+    result += f'\nETI{printETI}:\n'
     print(f'\nETI{printETI}:')
     printETI += 1
     switch(tempCode[8:-1])
 
     if code[0] == '[21]':
+        result += f'    jmp ETI{contEti}:\n'
         print(f'    jmp ETI{contEti}:')
         contEti += 1
         assemblySino(code)
+
+        result += f'\nETI{printETI}:\n'
         print(f'\nETI{printETI}:')
         printETI += 1
     else:
+        result += f'\nETI{printETI}:\n'
         print(f'\nETI{printETI}:')
         printETI += 1
 
 # Convierte la expresion del sino a codigo ensamblador
 def assemblySino(code):
     global printETI
+    global result
     tempCode = []
     brackets = 0
     # Este codigo revisa el inicio y fin del sino, para mandar solo eso a la funcion que crea el codigo de sino.
@@ -197,6 +285,7 @@ def assemblySino(code):
     for x in tempCode:
             code.remove(x)
 
+    result += f'\nETI{printETI}:\n'
     print(f'\nETI{printETI}:')
     printETI += 1
     switch(tempCode[3:-1])
@@ -206,6 +295,7 @@ def assemblySino(code):
 def assemblyMientras(code):
     global contEti
     global printETI
+    global result
     jumps = {'[07]':'je', '[08]':'jne', '[09]':'jg', '[10]':'jge', '[11]':'jl', '[12]':'jle',}
 
     tempCode = []
@@ -226,21 +316,38 @@ def assemblyMientras(code):
     for x in tempCode:
             code.remove(x)
 
+    result += f'    mov ax, {tempCode[2]}\n'
     print(f'    mov ax, {tempCode[2]}')
+
+    result += f'    mov ax, {tempCode[4]}\n'
     print(f'    mov bx, {tempCode[4]}')
+
+    result += f'    cmp ax, bx\n'
     print(f'    cmp ax, bx')
 
+    result += f'    {jumps[tempCode[2]]} ETI{contEti}\n'
     print(f'    {jumps[tempCode[2]]} ETI{contEti}')
     contEti += 1
+
+    result += f'    jmp ETI{contEti}\n'
     print(f'    jmp ETI{contEti}')
     contEti += 1
 
+    result += f'\nETI{printETI}:\n'
     print(f'\nETI{printETI}:')
     printETI += 1
     switch(tempCode[10:-1])
+
+    result += f'    mov ax, {tempCode[2]}\n'
     print(f'    mov ax, {tempCode[2]}')
+
+    result += '    add ax, 1\n'
     print('    add ax, 1')
+
+    result += f'    mov {tempCode[2]}, ax\n'
     print(f'    mov {tempCode[2]}, ax')
+
+    result += f'\nETI{printETI}:\n'
     print(f'\nETI{printETI}:')
     printETI += 1
 
@@ -248,6 +355,7 @@ def assemblyMientras(code):
 def assemblyDesde(code):
     global contEti
     global printETI
+    global result
 
     tempCode = []
     brackets = 0
@@ -267,21 +375,38 @@ def assemblyDesde(code):
     for x in tempCode:
             code.remove(x)
 
+    result += f'    mov ax, {tempCode[2]}\n'
     print(f'    mov ax, {tempCode[2]}')
+
+    result += f'    mov bx, {tempCode[4]}\n'
     print(f'    mov bx, {tempCode[4]}')
+
+    result += f'    cmp ax, bx\n'
     print(f'    cmp ax, bx')
 
+    result += f'    jle ETI{contEti}\n'
     print(f'    jle ETI{contEti}')
     contEti += 1
+
+    result += f'    jmp ETI{contEti}\n'
     print(f'    jmp ETI{contEti}')
     contEti += 1
 
+    result += f'\nETI{printETI}:\n'
     print(f'\nETI{printETI}:')
     printETI += 1
     switch(tempCode[10:-1])
+
+    result += f'    mov ax, {tempCode[2]}\n'
     print(f'    mov ax, {tempCode[2]}')
+
+    result += '    add ax, 1\n'
     print('    add ax, 1')
+
+    result += f'    mov {tempCode[2]}, ax\n'
     print(f'    mov {tempCode[2]}, ax')
+
+    result += f'\nETI{printETI}:\n'
     print(f'\nETI{printETI}:')
     printETI += 1
 
@@ -310,14 +435,17 @@ def switch(code):
         pass
 
 def main():
-
     global contEti
+    global result
     assemblyVariables()
     code = cargarCodigo()
     while len(code) > 0:
         switch(code)
 
+    result += '\n    int 20H'
     print('\n    int 20H')
+
+    print(result)
 
 if __name__=="__main__":
     main()
